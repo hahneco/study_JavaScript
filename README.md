2c16e8c8a935ae5b81277bed79e886769ccb977e
akiko_kuroda <hahnecochito@gmail.com>
2023-06-22 21:25:45 +0900

    📝初期ファイル追加

---
+++ README.md
@@ -0,0 +1,2 @@
+# codeSandBox_study
+Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+// 配列の展開
+const arr1 = [1, 2];
+console.log(arr1);
